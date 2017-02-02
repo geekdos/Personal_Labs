995b4fb58360b11c6a3f0731959ef254d53972b6
KHACHIAI Oussama <oussama.khachiai@gmail.com>
2017-02-02 19:45:58 +0000

    Junit 4.X asserts

---
+++ JavaLab/workspaceEclipseNew/JUnit4Assertions/src/testes/OperationsTest.java
@@ -0,0 +1,30 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import classes.Operations;
+
+public class OperationsTest {
+
+	@Test
+	public void additionAvecDeuxNombres() {
+		final long lAddition = Operations.additionner(10, 20);
+		assertEquals(30, lAddition);
+	}
+
+	@Test
+	public void additionAvecCinqNombres() {
+		final long lAddition = Operations.additionner(256, 512, 1024, 2048, 4096);
+		assertEquals(7936, lAddition);
+	}
+
+	@Test
+	public void mutiplicationAvecDeuxNombres() {
+		final long lMultiplication = Operations.multiplier(10, 20);
+		
+		assertEquals(200, lMultiplication);
+	}
+
+}
